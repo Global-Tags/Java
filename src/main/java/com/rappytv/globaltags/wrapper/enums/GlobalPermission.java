3bd7825ec25ef7550ad567134f2ac57e947aa28f
RappyTV <rappytv@rappytv.com>
2024-07-31 20:52:35 +0200

    Add PlayerInfo#hasPermission

---
+++ src/main/java/com/rappytv/globaltags/wrapper/enums/GlobalPermission.java
@@ -0,0 +1,10 @@
+package com.rappytv.globaltags.wrapper.enums;
+
+public enum GlobalPermission {
+    BypassValidation,
+    ManageBans,
+    ManageRoles,
+    ManageTags,
+    ManageWatchlist,
+    ReportImmunity
+}
