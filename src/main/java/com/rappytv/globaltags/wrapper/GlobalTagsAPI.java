ddfbbc73c3e91d8e509e73b564355e0c8dee812b
RappyTV <rappytv@rappytv.com>
2024-09-27 15:00:32 +0200

    Make GlobalTagsAPI an abstract class Add default values for cache and api handler

--- src/main/java/com/rappytv/globaltags/wrapper/GlobalTagsAPI.java
+++ src/main/java/com/rappytv/globaltags/wrapper/GlobalTagsAPI.java
@@ -9,17 +9,26 @@ import org.jetbrains.annotations.Nullable;
 import java.util.UUID;
 
 /**
- * A util interface for the GlobalTags API
+ * A util class for the GlobalTags API
  * @param <T> The result type of the {@link #translateColorCodes(String)} method
  */
-public interface GlobalTagsAPI<T> {
+public abstract class GlobalTagsAPI<T> {
+
+    /**
+     * Default cache options
+     */
+    private final PlayerInfo.Cache<T> cache = new PlayerInfo.Cache<>(this);
+    /**
+     * Default API handler
+     */
+    private final ApiHandler<T> apiHandler = new ApiHandler<>(this);
 
     /**
      * Get the API base for the GlobalTags API. Implementation note: Please don't add a trailing slash
      * @return The Globaltags API base
      */
     @NotNull
-    default String getApiBase() {
+    public String getApiBase() {
         return "https://api.globaltags.xyz";
     }
 
@@ -31,14 +40,14 @@ public interface GlobalTagsAPI<T> {
      * @return The user agent
      */
     @NotNull
-    Agent getAgent();
+    public abstract Agent getAgent();
 
     /**
      * Get the language which is sent to the API on any request
      * @return The language for any API responses
      */
     @NotNull
-    default String getLanguageCode() {
+    public String getLanguageCode() {
         return "en_us";
     }
 
@@ -48,49 +57,53 @@ public interface GlobalTagsAPI<T> {
      * @return A colored {@link T} representation of the input parameter
      */
     @NotNull
-    T translateColorCodes(@Nullable String input);
+    public abstract T translateColorCodes(@Nullable String input);
 
     /**
      * If this runs on the client side this will return a client UUID
      * @return The UUID of the current client
      */
     @Nullable
-    UUID getClientUUID();
+    public abstract UUID getClientUUID();
 
     /**
      * Get the tag cache
      * @return Returns a instance of {@link PlayerInfo.Cache}
      */
     @NotNull
-    PlayerInfo.Cache<T> getCache();
+    public PlayerInfo.Cache<T> getCache() {
+        return cache;
+    }
 
     /**
      * Get the api handler
      * @return Returns a instance of {@link ApiHandler}
      */
     @NotNull
-    ApiHandler<T> getApiHandler();
+    public ApiHandler<T> getApiHandler() {
+        return apiHandler;
+    }
 
     /**
      * Get the authentication type used to authenticate with the api
      * @return The authentication type used to authenticate with the api
      */
     @NotNull
-    AuthProvider getAuthType();
+    public abstract AuthProvider getAuthType();
 
     /**
      * Get the authorization to send to the API
      * @return The current authorization key to authenticate with the API
      */
     @Nullable
-    String getAuthorization();
+    public abstract String getAuthorization();
 
     /**
      * Get the authorization header
      * @return The authorization header for the API.
      */
     @NotNull
-    default String getAuthorizationHeader() {
+    public String getAuthorizationHeader() {
         String auth = getAuthorization();
         if(auth == null) return "";
         return String.format("%s %s", getAuthType().getId(), auth);
@@ -99,7 +112,7 @@ public interface GlobalTagsAPI<T> {
     /**
      * A class representing the request agent for better identification on behalf of the API
      */
-    class Agent {
+    public static class Agent {
 
         private final String agent;
         private final String agentVersion;
