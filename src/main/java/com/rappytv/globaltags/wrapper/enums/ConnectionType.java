ee2394de85f8622e4bb327920af68f11881f8f77
RappyTV <rappytv@rappytv.com>
2024-10-26 05:19:22 +0200

    Add missing GlobalPermission#CUSTOM_ICON, add javadocs to GlobalPermission and ConnectionType

--- src/main/java/com/rappytv/globaltags/wrapper/enums/ConnectionType.java
+++ src/main/java/com/rappytv/globaltags/wrapper/enums/ConnectionType.java
@@ -1,5 +1,8 @@
 package com.rappytv.globaltags.wrapper.enums;
 
+/**
+ * This enum includes types of accounts which can be linked to the GlobalTags account of a player.
+ */
 public enum ConnectionType {
     DISCORD,
     EMAIL
--- src/main/java/com/rappytv/globaltags/wrapper/enums/GlobalPermission.java
+++ src/main/java/com/rappytv/globaltags/wrapper/enums/GlobalPermission.java
@@ -1,7 +1,11 @@
 package com.rappytv.globaltags.wrapper.enums;
 
+/**
+ * This enum contains all GlobalTags permissions a player can have.
+ */
 public enum GlobalPermission {
     BYPASS_VALIDATION,
+    CUSTOM_ICON,
     MANAGE_BANS,
     MANAGE_ROLES,
     MANAGE_TAGS,
