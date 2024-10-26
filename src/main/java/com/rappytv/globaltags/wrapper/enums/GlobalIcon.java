00da312fafa67f411066ed3214ec6819b8951bbf
RappyTV <rappytv@rappytv.com>
2024-10-26 05:03:55 +0200

    Remove methods to get icon or role urls directly in their enum

--- src/main/java/com/rappytv/globaltags/wrapper/enums/GlobalIcon.java
+++ src/main/java/com/rappytv/globaltags/wrapper/enums/GlobalIcon.java
@@ -1,10 +1,5 @@
 package com.rappytv.globaltags.wrapper.enums;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.UUID;
-
 /**
  * This enum represents all valid icons which the official cdn supports.
  */
@@ -42,38 +37,5 @@ public enum GlobalIcon {
     TWITCH,
     X,
     XBOX,
-    YOUTUBE;
-
-    private final String iconUrl;
-
-    GlobalIcon() {
-        this.iconUrl = String.format(
-                "https://cdn.rappytv.com/globaltags/icons/%s.png",
-                this.name().toLowerCase()
-        );
-    }
-
-    /**
-     * Get the icon url of the {@link GlobalIcon}
-     * @return The icon url or null if the {@link GlobalIcon} equals {@link GlobalIcon#NONE}
-     */
-    @Nullable
-    public String getIconUrl() {
-        return this != NONE ? iconUrl : null;
-    }
-
-    /**
-     * Get the icon url of a custom player-uploaded icon
-     * @param uuid The player's uuid
-     * @param hash The icon's hash
-     * @return The icon url of a custom player-uploaded icon
-     */
-    @NotNull
-    public static String getCustomIconUrl(UUID uuid, String hash) {
-        return String.format(
-                "https://api.globaltags.xyz/players/%s/icon/%s",
-                uuid,
-                hash
-        );
-    }
+    YOUTUBE
 }
--- src/main/java/com/rappytv/globaltags/wrapper/enums/GlobalRole.java
+++ src/main/java/com/rappytv/globaltags/wrapper/enums/GlobalRole.java
@@ -1,7 +1,5 @@
 package com.rappytv.globaltags.wrapper.enums;
 
-import org.jetbrains.annotations.NotNull;
-
 /**
  * This enum represents all default roles and their icon urls which are configured in the <a href="https://github.com/Global-Tags/API/blob/master/config.json.example">API config file</a>.
  */
@@ -10,23 +8,5 @@ public enum GlobalRole {
     DEVELOPER,
     MODERATOR,
     PARTNER,
-    SUPPORTER;
-
-    private final String iconUrl;
-
-    GlobalRole() {
-        this.iconUrl = String.format(
-                "https://cdn.rappytv.com/globaltags/icons/role/%s.png",
-                name().toLowerCase()
-        );
-    }
-
-    /**
-     * Get the staff icon url of the {@link GlobalRole}
-     * @return The staff icon url of the {@link GlobalRole}
-     */
-    @NotNull
-    public String getIconUrl() {
-        return iconUrl;
-    }
+    SUPPORTER
 }
