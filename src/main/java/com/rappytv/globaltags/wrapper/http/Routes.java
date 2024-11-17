07ee9c9d6e2be8f0c2192f29f2c38eef89f55a47
RappyTV <rappytv@rappytv.com>
2024-11-17 18:02:36 +0100

    Format javadocs

--- src/main/java/com/rappytv/globaltags/wrapper/http/Routes.java
+++ src/main/java/com/rappytv/globaltags/wrapper/http/Routes.java
@@ -19,6 +19,7 @@ public class Routes {
      * Route for
      * - GET /
      * </pre>
+     *
      * @return The HTTP route
      */
     public static String getVersion() {
@@ -30,6 +31,7 @@ public class Routes {
      * Route for
      * - GET /metrics
      * </pre>
+     *
      * @return The HTTP route
      */
     public static String getMetrics() {
@@ -43,6 +45,7 @@ public class Routes {
      * - POST /players/{uuid}
      * - DELETE /players/{uuid}
      * </pre>
+     *
      * @param uuid The {@link UUID} of the player you want to get, set or delete the tag of
      * @return The HTTP route
      */
@@ -55,6 +58,7 @@ public class Routes {
      * Route for
      * - POST /players/{uuid}/position
      * </pre>
+     *
      * @param uuid The {@link UUID} of the player you want to set the position of
      * @return The HTTP route
      */
@@ -67,6 +71,7 @@ public class Routes {
      * Route for
      * - POST /players/{uuid}/icon
      * </pre>
+     *
      * @param uuid The {@link UUID} of the player you want to set the icon of
      * @return The HTTP route
      */
@@ -79,6 +84,7 @@ public class Routes {
      * Route for
      * - POST /players/{uuid}/watch
      * </pre>
+     *
      * @param uuid The {@link UUID} of the player you want to watch
      * @return The HTTP route
      */
@@ -91,6 +97,7 @@ public class Routes {
      * Route for
      * - POST /players/{uuid}/unwatch
      * </pre>
+     *
      * @param uuid The {@link UUID} of the player you want to unwatch
      * @return The HTTP route
      */
@@ -103,6 +110,7 @@ public class Routes {
      * Route for
      * - POST /players/{uuid}/refer
      * </pre>
+     *
      * @param uuid The {@link UUID} of the player you want to refer to
      * @return The HTTP route
      */
@@ -115,6 +123,7 @@ public class Routes {
      * Route for
      * - POST /players/{uuid}/report
      * </pre>
+     *
      * @param uuid The {@link UUID} of the player you want to report
      * @return The HTTP route
      */
@@ -128,6 +137,7 @@ public class Routes {
      * - POST /players/{uuid}/connections/discord
      * - DELETE /players/{uuid}/connections/discord
      * </pre>
+     *
      * @param uuid The {@link UUID} of the player you want to set/delete the connection of
      * @param type The connection type
      * @return The HTTP route
@@ -141,6 +151,7 @@ public class Routes {
      * Route for
      * - POST /players/{uuid}/connections/email/{code}
      * </pre>
+     *
      * @param uuid The {@link UUID} of the player you want to verify the email of
      * @param code The verification code which was received via email
      * @return The HTTP route
@@ -157,6 +168,7 @@ public class Routes {
      * - PUT /players/{uuid}/ban
      * - DELETE /players/{uuid}/ban
      * </pre>
+     *
      * @param uuid The {@link UUID} of the player you want to manage the ban of
      * @return The HTTP route
      */
@@ -169,6 +181,7 @@ public class Routes {
      * Route for
      * - POST /players/{uuid}/ban/appeal
      * </pre>
+     *
      * @param uuid The {@link UUID} of the player you want to appeal the ban of
      * @return The HTTP route
      */
@@ -182,6 +195,7 @@ public class Routes {
      * - GET /players/{uuid}/notes
      * - POST /players/{uuid}/notes
      * </pre>
+     *
      * @param uuid The player you want to get or create notes for
      * @return The HTTP route
      */
@@ -195,6 +209,7 @@ public class Routes {
      * - GET /players/{uuid}/notes/{id}
      * - DELETE /players/{uuid}/notes/{id}
      * </pre>
+     *
      * @param uuid The player you want to get or delete a note for
      * @param id The note ID
      * @return The HTTP route
